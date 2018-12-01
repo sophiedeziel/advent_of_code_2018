bb99fac27839d68c9b053da86410c2e3e89a8596
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-01 13:44:30 -0500

    ajouter quelques commentaires

--- jour1/chronal_calibration.rb
+++ jour1/chronal_calibration.rb
@@ -3,16 +3,21 @@ frequency_history = {0 => true}
 double = nil
 first_run = true
 
+# on ouvre le fichier et on le place en mémoire
 lines = File.open('input.txt', 'r').map { |line| line }
-#lines = File.open('input.txt', 'r')
 
+# on exécute tant qu'on n'a pas trouvé un doublon
 while(double.nil?) do
   lines.each do |line|
     frequency += line.to_i
+
+    # On sort de la boucle si on trouve un doublon dans la liste
     if frequency_history.has_key? frequency
       double = frequency
       break
     end
+
+    # On insère le résultat dans la liste
     frequency_history.merge! Hash[frequency, true]
   end
 
