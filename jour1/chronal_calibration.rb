911ceaa9df6a2363ebfe7db293c80de33e698316
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-01 13:34:32 -0500

    using a hash improves drastically performance

--- jour1/chronal_calibration.rb
+++ jour1/chronal_calibration.rb
@@ -1,16 +1,19 @@
 frequency = 0
-frequency_history = [0]
+frequency_history = {0 => true}
 double = nil
 first_run = true
 
+lines = File.open('input.txt', 'r').map { |line| line }
+#lines = File.open('input.txt', 'r')
+
 while(double.nil?) do
-  File.open('input.txt', 'r').each do |line|
+  lines.each do |line|
     frequency += line.to_i
-    if frequency_history.include? frequency
+    if frequency_history.has_key? frequency
       double = frequency
       break
     end
-    frequency_history << frequency
+    frequency_history.merge! Hash[frequency, true]
   end
 
   puts "Part 1: #{frequency}" if first_run
