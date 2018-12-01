cfe4b3054107af7ea99f794f8fe73eb3a480b843
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-01 13:16:29 -0500

    part two of day one

--- jour1/chronal_calibration.rb
+++ jour1/chronal_calibration.rb
@@ -1,3 +1,20 @@
 frequency = 0
-File.open('input.txt', 'r').each { |line| frequency += line.to_i }
-puts frequency
+frequency_history = [0]
+double = nil
+first_run = true
+
+while(double.nil?) do
+  File.open('input.txt', 'r').each do |line|
+    frequency += line.to_i
+    if frequency_history.include? frequency
+      double = frequency
+      break
+    end
+    frequency_history << frequency
+  end
+
+  puts "Part 1: #{frequency}" if first_run
+  first_run = false
+end
+
+puts "Part 2: #{double}"
