f761c6c915542521b6406b616b7dad8a8f35bc46
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-02 14:17:30 -0500

    Part 2

--- jour2/inventory_management_system.rb
+++ jour2/inventory_management_system.rb
@@ -1,4 +1,23 @@
-lines = File.open('input.txt', 'r').map(&:to_s)
+lines = []
+boxes_ids = ''
+
+def compare(string1, string2)
+  differences = 0
+  string1.chars.each_with_index do |char, i|
+    differences += 1 if char != string2[i]
+  end
+  differences
+end
+
+File.open('input.txt', 'r').each do |line|
+  similar = lines.find { |saved_line| compare(line, saved_line) == 1 }
+  if similar
+    line.chars.each_with_index do |value, i|
+      boxes_ids += value if value == similar[i]
+    end
+  end
+  lines << line
+end
 
 double_count = 0
 triple_count = 0
@@ -14,3 +33,5 @@ lines.each do |line|
 end
 
 puts "Part 1: #{double_count * triple_count}"
+puts "Part 2: #{boxes_ids}"
+
