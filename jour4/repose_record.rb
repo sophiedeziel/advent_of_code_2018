48430732e599f007c9de2c93969ac0f453a8f340
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-04 23:10:32 -0500

    abuse procs

--- jour4/repose_record.rb
+++ jour4/repose_record.rb
@@ -1,46 +1,20 @@
 require 'pry'
 require 'awesome_print'
+require_relative 'schedule_parser.rb'
 
-records = File.open('input.txt', 'r').map(&:to_s)
-schedule = {}
-current_guard = nil
-current_sleep = nil
+schedule = ScheduleParser.new('input.txt').parse
 
-@hours_asleep = -> (array) { array.flatten.select { |v| v }.count }
+@hours_asleep       = -> (array)            { array.flatten.select { |v| v }.count }
+@map_sleep          = -> (schedule, method) { schedule.map { |key, array| [key, method.call(array)] } }
 
-records.sort.each do |line|
-  if shift  = line.match(/Guard #(?<id>\d+) begins shift/)
-    current_guard = shift[:id]
-    current_sleep = nil
-  end
-
-  if fall_asleep = line.match(/\[.+:(?<minute>\d+)\] falls asleep/)
-    current_sleep = fall_asleep[:minute].to_i
-  end
-
-  if wakes_up = line.match(/\[.+:(?<minute>\d+)\] wakes up/)
-    current_wake = wakes_up[:minute].to_i
-    schedule[current_guard] ||= []
-    schedule[current_guard] << (0...59).map {|i| (current_sleep...current_wake).include?(i)}
-  end
+@most_asleep_minute = -> (guards_schedule) do
+  list = guards_schedule.transpose.each_with_index.map{|k,v| [v,k]}
+  @map_sleep.call(list, @hours_asleep).sort_by {|k, v| v}.last
 end
 
-biggest_sleeper = schedule.map do |guard, hours|
-  [guard, @hours_asleep.call(hours)]
-end.sort_by {|k, v| v}.last
+biggest_sleeper         = @map_sleep.call(schedule, @hours_asleep).sort_by {|k, v| v}.last
+most_consistent_sleeper = @map_sleep.call(schedule, @most_asleep_minute).sort_by {|k, v| v[1]}.last
 
-def most_asleep_minute(guards_schedule)
-  guards_schedule.transpose.each_with_index.map do |array, index|
-    [index, @hours_asleep.call(array)]
-  end.sort_by {|k, v| v}.last
-end
-
-most_consistent_sleeper =
-schedule.map do |guard, hours|
-  [guard, most_asleep_minute(hours)]
-end.sort_by {|k, v| v[1]}.last
-
-
-ap "Part 1: #{ biggest_sleeper[0].to_i * most_asleep_minute(schedule[biggest_sleeper[0]])[0] }"
+ap "Part 1: #{ biggest_sleeper[0].to_i * @most_asleep_minute.call(schedule[biggest_sleeper[0]])[0] }"
 ap "Part 2: #{most_consistent_sleeper[0].to_i * most_consistent_sleeper[1][0]}"
 
---
+++ jour4/schedule_parser.rb
@@ -0,0 +1,46 @@
+class ScheduleParser
+  attr_accessor :current_guard, :current_sleep, :schedule
+
+  SHIFT_PATTERN       = /Guard #(?<id>\d+) begins shift/
+  FALL_ASLEEP_PATTERN = /\[.+:(?<minute>\d+)\] falls asleep/
+  WAKES_PATTERN       = /\[.+:(?<minute>\d+)\] wakes up/
+
+  def initialize(file_name)
+    @lines = File.open(file_name, 'r').map(&:to_s)
+  end
+
+  def parse
+    @schedule = {}
+
+    @lines.sort.each do |line|
+      check_shift(line)
+      check_falls(line)
+      check_wake(line)
+    end
+
+    @schedule
+  end
+
+  private
+
+  def check_shift(line)
+    if shift = line.match(SHIFT_PATTERN)
+      @current_guard = shift[:id]
+      @current_sleep = nil
+    end
+  end
+
+  def check_falls(line)
+    if fall_asleep = line.match(FALL_ASLEEP_PATTERN)
+      @current_sleep = fall_asleep[:minute].to_i
+    end
+  end
+
+  def check_wake(line)
+    if wakes_up = line.match(WAKES_PATTERN)
+      @current_wake = wakes_up[:minute].to_i
+      @schedule[@current_guard] ||= []
+      @schedule[@current_guard] << (0...59).map {|i| (@current_sleep...@current_wake).include?(i)}
+    end
+  end
+end
