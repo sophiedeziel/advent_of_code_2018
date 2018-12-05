f08c7034fe970c51371071a8961b2a887e1de86c
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-04 23:55:22 -0500

    slight improvements

--- jour4/repose_record.rb
+++ jour4/repose_record.rb
@@ -1,15 +1,20 @@
 require_relative 'schedule_parser.rb'
 
-@biggest_sleeper         = -> { @highest.(@map_sleep.(schedule, @hours_asleep)) }
+@biggest_sleeper         = -> { @highest.(@map_sleep.(schedule, @minutes_asleep)) }
 @most_consistent_sleeper = -> { @highest.(@map_sleep.(schedule, @most_asleep_minute).map{ |k,v| [k, v.reverse] }) }
 
-@hours_asleep       = -> (array)            { array.flatten.select { |v| v }.count }
+@minutes_asleep     = -> (array)            { array.flatten.count(true) }
 @map_sleep          = -> (schedule, method) { schedule.map { |key, array| [key, method.call(array)] } }
 @highest            = -> (array)            { array.sort_by {|k, v| v}.last }
 
 @most_asleep_minute = -> (guards_schedule) do
-  list = guards_schedule.transpose.each_with_index.map{|k,v| [v,k]}
-  @highest.(@map_sleep.call(list, @hours_asleep))
+  list =
+    guards_schedule
+    .transpose
+    .each_with_index
+    .to_a
+    .map(&:reverse)
+  @highest.(@map_sleep.call(list, @minutes_asleep))
 end
 
 def strategy_1
@@ -26,5 +31,5 @@ def schedule
   @schedule ||= ScheduleParser.new('input.txt').parse
 end
 
-print "Part 1: #{ strategy_1.reduce(:*) }"
-print "Part 2: #{ strategy_2.reduce(:*) }"
+puts "Part 1: #{ strategy_1.reduce(:*) }"
+puts "Part 2: #{ strategy_2.reduce(:*) }"
