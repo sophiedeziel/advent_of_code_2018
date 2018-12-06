3d9d891bfd0d783378e99f721b15bade77f60e9f
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-05 21:29:42 -0500

    part 2

--- jour5/alchemical_reduction.rb
+++ jour5/alchemical_reduction.rb
@@ -1,12 +1,27 @@
 final_string = []
+input = File.read('input.txt').to_s.tap {|s| s.gsub!("\n", '')}
 
-File.open('input.txt', 'r').each_char do |char|
-  last = final_string.pop
+def reduce_polymer(input_string)
+  final_string = []
+  input_string.each_char do |char|
+    last = final_string.pop
 
-  next if last&.downcase == char.downcase && last != char
+    next if last&.downcase == char.downcase && last != char
 
-  final_string.push last if last
-  final_string.push char if char != "\n"
+    final_string.push last if last
+    final_string.push char
+  end
+
+  final_string.join.size
 end
 
-puts "Part 1 #{final_string.join.size }"
+experiments = ('a'...'z').map do |unit|
+  test_string = input.dup
+  test_string.gsub!(unit, '')
+  test_string.gsub!(unit.upcase, '')
+
+  reduce_polymer(test_string)
+end
+
+puts "Part 1 #{reduce_polymer(input)}"
+puts "Part 2 #{experiments.min}"
