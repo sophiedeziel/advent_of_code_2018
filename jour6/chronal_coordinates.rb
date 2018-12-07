303ca2fcdff0ac0a76d53c2c0b771fab7d24b08b
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-06 22:16:35 -0500

    Part 2

--- jour6/chronal_coordinates.rb
+++ jour6/chronal_coordinates.rb
@@ -1,3 +1,5 @@
+require 'pry'
+require 'awesome_print'
 points = File.open('input.txt').each_with_index.map {|point, index| point.split(', ').map(&:to_i).unshift(index) }
 
 top    = points.min_by { |id, x, y| y }[2] - 1
@@ -13,11 +15,16 @@ def calculate_manhattan_distance(ax, ay, bx, by)
   (ax - bx).abs + (ay - by).abs
 end
 
+safe_region = 0
 map.each do |coordinate, distances|
   map[coordinate] ||= {}
   points.each do |id, x, y|
     map[coordinate][id] = calculate_manhattan_distance(x,y,coordinate[0], coordinate[1])
   end
+
+  sum = map[coordinate].values.reduce(:+)
+  safe_region +=1 if sum < 10_000
+
   sorted = map[coordinate].sort_by { |id, value|  value }
   if sorted[0][1] == sorted[1][1]
     map[coordinate][:min] = nil
@@ -34,3 +41,5 @@ end
 
 highest = finite.sort_by { |id, points| points.count }.last
 puts "Part 1: #{ highest[1].count }"
+puts "Part 2: #{ safe_region }"
+
