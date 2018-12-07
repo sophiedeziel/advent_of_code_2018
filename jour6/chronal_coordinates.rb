8b7177ee9e97acf317e972d03f9141565cb3706c
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-06 21:58:23 -0500

    part 1

---
+++ jour6/chronal_coordinates.rb
@@ -0,0 +1,36 @@
+points = File.open('input.txt').each_with_index.map {|point, index| point.split(', ').map(&:to_i).unshift(index) }
+
+top    = points.min_by { |id, x, y| y }[2] - 1
+bottom = points.max_by { |id, x, y| y }[2] + 1
+left   = points.min_by { |id, x, y| x }[1] - 1
+right  = points.max_by { |id, x, y| x }[1] + 1
+
+map = {}
+
+(left..right).flat_map { |x| (top..bottom).each { |y| map[[x,y]] = nil } }
+
+def calculate_manhattan_distance(ax, ay, bx, by)
+  (ax - bx).abs + (ay - by).abs
+end
+
+map.each do |coordinate, distances|
+  map[coordinate] ||= {}
+  points.each do |id, x, y|
+    map[coordinate][id] = calculate_manhattan_distance(x,y,coordinate[0], coordinate[1])
+  end
+  sorted = map[coordinate].sort_by { |id, value|  value }
+  if sorted[0][1] == sorted[1][1]
+    map[coordinate][:min] = nil
+  else
+    map[coordinate][:min] = sorted.first[0]
+  end
+end
+
+finite = map.group_by { |coordinate, distances| distances[:min] }.reject do |area, points|
+  points.any? do |coordinates, distances|
+    [left, right].include?(coordinates[0]) || [top, bottom].include?(coordinates[1])
+  end
+end
+
+highest = finite.sort_by { |id, points| points.count }.last
+puts "Part 1: #{ highest[1].count }"
