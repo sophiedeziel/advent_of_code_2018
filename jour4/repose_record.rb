4222c971f76d03f0c9b9ddaf3296140eea1772f8
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-06 19:48:53 -0500

    huuuuuge code improvement

--- jour4/repose_record.rb
+++ jour4/repose_record.rb
@@ -1,35 +1,58 @@
-require_relative 'schedule_parser.rb'
+require 'pry'
+require 'awesome_print'
 
-@biggest_sleeper         = -> { @highest.(@map_sleep.(schedule, @minutes_asleep)) }
-@most_consistent_sleeper = -> { @highest.(@map_sleep.(schedule, @most_asleep_minute).map{ |k,v| [k, v.reverse] }) }
+class Guard
+  attr_accessor :id, :minutes_asleep, :asleep_by_minutes
+  def initialize(id)
+    @id = id
+    @minutes_asleep = 0
+    @asleep_by_minutes = (0...59).map { 0 }
+  end
 
-@minutes_asleep     = -> (array)            { array.flatten.count(true) }
-@map_sleep          = -> (schedule, method) { schedule.map { |key, array| [key, method.call(array)] } }
-@highest            = -> (array)            { array.sort_by {|k, v| v}.last }
+  def register_sleep(fallen, woke_up)
+    (fallen...woke_up).each do |i|
+      @asleep_by_minutes[i] +=1
+      @minutes_asleep += 1
+    end
+  end
 
-@most_asleep_minute = -> (guards_schedule) do
-  list =
-    guards_schedule
-    .transpose
-    .each_with_index
-    .to_a
-    .map(&:reverse)
-  @highest.(@map_sleep.call(list, @minutes_asleep))
+  def most_asleep_minute
+    @asleep_by_minutes.each_with_index.max[1]
+  end
 end
 
-def strategy_1
-  biggest_sleeper = @biggest_sleeper.()
-  [biggest_sleeper[0], @most_asleep_minute.(schedule[biggest_sleeper[0]])[0]]
+records = File.read('input.txt').split("\n")
+guards = []
+current_guard = nil
+current_sleep = nil
+
+records.sort.each do |line|
+  if shift  = line.match(/Guard #(?<id>\d+) begins shift/)
+    id = shift[:id].to_i
+
+    unless current_guard = guards.find { |g| g.id == id }
+      current_guard = Guard.new(id)
+      guards << current_guard
+    end
+
+    current_sleep = nil
+  end
+
+  if fall_asleep = line.match(/\[.+:(?<minute>\d+)\] falls asleep/)
+    current_wake = nil
+    current_sleep = fall_asleep[:minute].to_i
+  end
+
+  if wakes_up = line.match(/\[.+:(?<minute>\d+)\] wakes up/)
+    current_wake = wakes_up[:minute].to_i
+    current_guard.register_sleep(current_sleep, current_wake)
+  end
 end
 
-def strategy_2
-  most_consistent_sleeper = @most_consistent_sleeper.()
-  [most_consistent_sleeper[0], most_consistent_sleeper[1][1]]
-end
+biggest_sleeper = guards.max_by { |guard| guard.minutes_asleep }
+puts "Part 1: #{biggest_sleeper.id * biggest_sleeper.most_asleep_minute }"
 
-def schedule
-  @schedule ||= ScheduleParser.new('input.txt').parse
-end
+most_consistent_sleeper = guards.max_by { |guard| guard.asleep_by_minutes[guard.most_asleep_minute] }
+puts "Part 2: #{most_consistent_sleeper.id * most_consistent_sleeper.most_asleep_minute}"
 
-puts "Part 1: #{ strategy_1.reduce(:*) }"
-puts "Part 2: #{ strategy_2.reduce(:*) }"
+
