438e076c79457e7e984476b7d88fb0f34c5715b3
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-06 22:33:25 -0500

    slight improvements

--- jour6/chronal_coordinates.rb
+++ jour6/chronal_coordinates.rb
@@ -1,25 +1,26 @@
-require 'pry'
-require 'awesome_print'
-points = File.open('input.txt').each_with_index.map {|point, index| point.split(', ').map(&:to_i).unshift(index) }
+Point = Struct.new(:x, :y)
+points = File.open('input.txt').each_with_index.map do |point, index|
+  [index, Point.new(*point.split(', ').map(&:to_i)) ]
+end
 
-top    = points.min_by { |id, x, y| y }[2] - 1
-bottom = points.max_by { |id, x, y| y }[2] + 1
-left   = points.min_by { |id, x, y| x }[1] - 1
-right  = points.max_by { |id, x, y| x }[1] + 1
+top    = points.min_by { |id, point| point.y }[1].y - 1
+bottom = points.max_by { |id, point| point.y }[1].y + 1
+left   = points.min_by { |id, point| point.x }[1].x - 1
+right  = points.max_by { |id, point| point.x }[1].x + 1
 
 map = {}
 
-(left..right).flat_map { |x| (top..bottom).each { |y| map[[x,y]] = nil } }
+(left..right).flat_map { |x| (top..bottom).each { |y| map[Point.new(x,y)] = nil } }
 
-def calculate_manhattan_distance(ax, ay, bx, by)
-  (ax - bx).abs + (ay - by).abs
+def calculate_manhattan_distance(point_a, point_b)
+  (point_a.x - point_b.x).abs + (point_a.y - point_b.y).abs
 end
 
 safe_region = 0
 map.each do |coordinate, distances|
   map[coordinate] ||= {}
-  points.each do |id, x, y|
-    map[coordinate][id] = calculate_manhattan_distance(x,y,coordinate[0], coordinate[1])
+  points.each do |id, point|
+    map[coordinate][id] = calculate_manhattan_distance(point,coordinate)
   end
 
   sum = map[coordinate].values.reduce(:+)
