0df7ab1040646ca3708ead3a02349aa344e22052
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-08 16:10:19 -0500

    use more methods to reduce complexity

--- jour7/sum_of_its_parts.rb
+++ jour7/sum_of_its_parts.rb
@@ -1,7 +1,13 @@
 require 'pry'
 require 'awesome_print'
 Node = Struct.new(:id, :children)
+
 @nodes = {}
+@answer = ''
+@workers = {}
+(1..5).each do |worker|
+  @workers[worker] = {current: nil, time: 0}
+end
 
 File.open('input.txt').each do |line|
   id    = line[5]
@@ -14,8 +20,6 @@ File.open('input.txt').each do |line|
   @nodes[id].children.sort!.reverse!
 end
 
-@answer = ''
-
 def available_nodes
   @nodes.reject do |id, node|
     @nodes.any? { |_id, n| n.children.include? id } || @workers.any? { |_id, worker| worker[:current] == id }
@@ -26,14 +30,6 @@ def available_workers
   @workers.select { |k,v| v[:current].nil? }
 end
 
-@workers = {}
-min_time = 60
-(1..5).each do |worker|
-  @workers[worker] = {current: nil, time: 0}
-end
-
-current_second = 0
-
 def assign_tasks
   available_workers.each do |id, worker|
     available = available_nodes.sort_by {|id, node| id }
@@ -41,25 +37,31 @@ def assign_tasks
   end
 end
 
-while @nodes.any?
-  current_second += 1
-  assign_tasks
+def mark_as_done(node_id)
+  @answer += node_id
+  @nodes.delete(node_id)
+  @nodes.each do |key, node|
+    node.children.delete(node_id)
+  end
+end
 
-  @workers.each do |id, worker|
-    if worker[:current]
-      worker[:time] += 1
-      if worker[:time] >= (min_time + worker[:current].ord - 64)
-        @answer += worker[:current]
-        @nodes.delete(worker[:current])
-        @nodes.each do |key, node|
-          node.children.delete(worker[:current])
-        end
-        worker[:current] = nil
-        worker[:time]    = 0
-      end
-    end
+def work(worker)
+  worker[:time] += 1
+  if worker[:time] >= worker[:current].ord - 4
+    mark_as_done(worker[:current])
+    worker[:current] = nil
+    worker[:time]    = 0
+  end
+end
+
+total_time = 0
+while @nodes.any?
+  total_time += 1
+  assign_tasks
+  @workers.reject{|id, worker| worker[:current].nil?}.each do |id, worker|
+    work(worker)
   end
 end
 
 puts "Part 1: #{@answer}"
-puts "Part 2: #{current_second}"
+puts "Part 2: #{total_time}"
