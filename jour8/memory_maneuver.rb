8f9e8f9c121d03c7ff481acaeb2896da5827e267
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-08 19:17:45 -0500

    Refactor to make it nice
    !
    \!

--- jour8/memory_maneuver.rb
+++ jour8/memory_maneuver.rb
@@ -1,45 +1,33 @@
-require 'pry'
-
-class Node
-  attr_accessor :nb_children, :nb_metadata, :children, :metadata
-  def initialize(nb_children, nb_metadata, children, metadata)
-    @nb_children, @nb_metadata, @children, @metadata = nb_children, nb_metadata, children, metadata
-  end
-
+class Node < Struct.new(:nb_children, :nb_metadata, :children, :metadata)
   def value
-    if @nb_children == 0
-      @metadata.reduce(:+)
-    else
-      sum = 0
-      @metadata.each do |index|
-        next unless @children[index-1]
-        sum += @children[index-1].value
-      end
-      sum
+    return metadata.reduce(:+) if nb_children == 0
+
+    sum = 0
+    metadata.each do |index|
+      next unless children[index-1]
+      sum += children[index-1].value
     end
+    sum
   end
 end
 
-input = File.read('input.txt').split.map(&:to_i)
-
-def get_node(array)
-  node = Node.new(0, 0, [], [])
-  node.nb_children = array.shift
-  node.nb_metadata = array.shift
+def parse_node(array)
+  node = Node.new(array.shift, array.shift, [], [])
 
   node.nb_children.times do
-    node.children << get_node(array)
+    node.children << parse_node(array)
   end
 
-  node.metadata =  array.shift(node.nb_metadata)
+  node.metadata = array.shift(node.nb_metadata)
   @total += node.metadata.reduce(:+)
 
   node
 end
 
+input = File.read('input.txt').split.map(&:to_i)
+
 @total = 0
-root = get_node(input)
+root = parse_node(input)
 
 puts "Part 1: #{ @total }"
 puts "Part 2: #{ root.value }"
-
