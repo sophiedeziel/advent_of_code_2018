c7fdc8f1e9d69e9eb1d7f19a3d6f2d7f0f9036ca
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-10 11:48:06 -0500

    Part 2

--- jour10/stars_align.rb
+++ jour10/stars_align.rb
@@ -24,7 +24,9 @@ def bounding_box_area
   (box[1].x - box[0].x) * (box[1].y - box[0].y)
 end
 
+seconds = -1
 begin
+  seconds += 1
   last_rectangle = bounding_box_area
   @points.each(&:move)
 end while(last_rectangle > bounding_box_area)
@@ -32,14 +34,7 @@ end while(last_rectangle > bounding_box_area)
 @points.each(&:revert)
 
 box = bounding_box
-output = []
-
-(0..(box[1].y - box[0].y )).each do |y|
-  output[y] ||= []
-  (0..(box[1].x - box[0].x )).each do |x|
-    output[y][x] ||= ' '
-  end
-end
+output = Array.new (box[1].y - box[0].y + 1) { Array.new((box[1].x - box[0].x + 1), ' ') }
 
 @points.each do |point|
   output[point.y - box[0].y][point.x - box[0].x] = "#"
@@ -48,3 +43,4 @@ end
 puts "Part 1:"
 puts output.map{|v| v.join('') }.join("\n")
 
+puts "Part 2: #{seconds}"
