e83bd1db2709cd1b307e45da1ef4427266e1887c
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-10 21:16:35 -0500

    a bit of a refactor to reduce variables assignments

--- jour09/marble_mania.rb
+++ jour09/marble_mania.rb
@@ -1,38 +1,40 @@
-require 'pry'
-#478 players; last marble is worth 71240 points
+class Marble
+  attr_accessor :value, :next, :previous
+  def initialize(value)
+    @value = value
+    @next  = self
+    @previous = self
+  end
 
-@num_players = 478
-highest_marble = 71240
-
-class Marble < Struct.new(:value, :previous, :next)
   def destroy
-    self.previous.next = self.next
-    self.next.previous = self.previous
+    @previous.next = @next
+    @next.previous = @previous
   end
 
   def insert_after(marble)
-    marble.next        = self.next
-    marble.previous    = self
-    self.next.previous = marble
-    self.next          = marble
+    marble.next     = @next
+    marble.previous = self
+    @next.previous  = marble
+    @next           = marble
   end
 end
 
-@players = (0...@num_players).map{0}
-@current_marble = Marble.new(0, nil, nil)
-@current_marble.next     = @current_marble
-@current_marble.previous = @current_marble
+num_players = 478
+highest_marble = 71240
+
+@players        = Array.new(num_players, 0)
+@current_marble = Marble.new(0)
 @value = 0
 
 def turn
   @value += 1
-  new_marble = Marble.new(@value, nil, nil)
+  new_marble = Marble.new(@value)
 
   if new_marble.value % 23 == 0
     7.times { @current_marble = @current_marble.previous }
     points = @current_marble.value + new_marble.value
 
-    @players[@value % @num_players] += points
+    @players[@value % @players.count] += points
     @current_marble.destroy
 
     @current_marble = @current_marble.next
@@ -47,10 +49,12 @@ end
 highest_marble.times do
   turn
 end
+
 puts "Part 1: #{@players.max}"
 
 (highest_marble * 100 - highest_marble).times do
   turn
 end
+
 puts "Part 2: #{@players.max}"
 
