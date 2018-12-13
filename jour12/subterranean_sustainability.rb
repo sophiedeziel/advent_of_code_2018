fd97a4472e6be8046803ef1449863837cccda5e6
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-12 22:38:30 -0500

    part 2

--- jour12/subterranean_sustainability.rb
+++ jour12/subterranean_sustainability.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-initial_state = '........#.##.###.#.##...##..#..##....#.#.#.#.##....##..#..####..###.####.##.#..#...#..######.#.....#..##...#.......'
+initial_state = '.#.##.###.#.##...##..#..##....#.#.#.#.##....##..#..####..###.####.##.#..#...#..######.#.....#..##...#'
 
 rules  = {}
 states = {}
@@ -12,21 +12,12 @@ end
 
 initial_state.split('').each_with_index do |char, index|
   value = char == '#'
-  states[index - 8] = value
+  states[index - 1] = value
 end
 
-puts "   " + states.map {|k, v| k % 10 == 0 ? k.digits.last : ' '}.compact.join('')
-puts "0: " + states.map {|k, v| k < -2 ? nil : v ? '#' : '.'}.compact.join('')
-
-(1..20).each do |g|
+(1..100).each do |g|
   new_gen = {}
   states.each do |pot, state|
-    new_gen[pot - 2] ||= false
-    new_gen[pot - 1] ||= false
-
-    new_gen[pot + 1] ||= false
-    new_gen[pot + 2] ||= false
-
     l2 = states[pot - 2] || false
     l1 = states[pot - 1] || false
 
@@ -36,10 +27,24 @@ puts "0: " + states.map {|k, v| k < -2 ? nil : v ? '#' : '.'}.compact.join('')
     neighbors  = [l2, l1, state, r1, r2]
     new_gen[pot] = rules[neighbors] || false
   end
+  last = new_gen.max_by { |k,v| k }
+  new_gen[last[0] + 1] = false if new_gen[last[0]]
+
+  first = new_gen.min_by { |k,v| k }
+  if new_gen[first[0]]
+    new_gen[first[0] - 1] = false
+  else
+    new_gen.delete(first[0]) unless new_gen[first[0] + 1]
+  end
+
   states = new_gen
-  puts g.to_s + ": " + states.map {|k, v|  v ? '#' : '.'}.compact.join('')
+  puts "Part 1: #{states.select { |k,v| v }.map { |k,v| k }.sum}" if g == 20
 end
 
+# En analysant les séquences, je me suis rendue compte qu'à la génération 100, le pattern se stabilise.
+# Le pattern ne fait qu'augmenter la somme par 62
 
-puts "Part 1: #{states.select { |k,v| v }.map { |k,v| k }.sum}"
+puts "Part 2: #{states.select { |k,v| v }.map { |k,v| k }.sum + (50000000000 - 100) * 62}"
+
+
 
