515a88c0f6696ec847989d6f98553de3c5048b13
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-15 00:31:10 -0500

    make it OO

--- jour12/subterranean_sustainability.rb
+++ jour12/subterranean_sustainability.rb
@@ -1,57 +1,75 @@
-require 'pry'
+class GameOfLife
+  attr_accessor :rules, :pots
 
-initial_state = '.#.##.###.#.##...##..#..##....#.#.#.#.##....##..#..####..###.####.##.#..#...#..######.#.....#..##...#'
+  def initialize
+    @rules = {}
+    @pots = {}
+  end
 
-rules  = {}
-states = {}
+  def cycle(generations)
+    known_sequences = []
+    score = 0
+
+    (1..generations).each do |g|
+      evolve
+
+      score = calc_score(@pots)
+      if known = known_sequences.find { |sequence| sequence.values == @pots.values }
+        score = score + (generations - g) * (score - calc_score(known))
+        break
+      else
+        known_sequences << @pots.dup
+      end
+    end
+    score
+  end
+
+  def evolve
+    new_gen = {}
+
+    clean
+    @pots.each do |pot, state|
+      l2 = @pots[pot - 2] || false
+      l1 = @pots[pot - 1] || false
+
+      r1 = @pots[pot + 1] || false
+      r2 = @pots[pot + 2] || false
+
+      neighbors  = [l2, l1, state, r1, r2]
+      new_gen[pot] = @rules[neighbors]
+    end
+
+    @pots = new_gen
+  end
+
+  def calc_score(generation)
+    generation.map { |k,v| k if v }.compact.sum
+  end
+
+  def clean
+    first = @pots.keys.min
+    @pots[first - 1] = false if @pots[first]
+    @pots.delete(first) unless @pots[first] || @pots[first + 1]
+
+    last = @pots.keys.max
+    @pots[last + 1] = false if @pots[last]
+    @pots.delete(last) unless @pots[last] || @pots[last - 1]
+  end
+end
+
+game = GameOfLife.new
 
 File.open('input.txt').each do |line|
   key = line[0...5].split('').map { |char| char == '#' }
-  rules[key] = line[9] == '#'
+  game.rules[key] = line[9] == '#'
 end
 
+initial_state = '#.##.###.#.##...##..#..##....#.#.#.#.##....##..#..####..###.####.##.#..#...#..######.#.....#..##...#'
 initial_state.split('').each_with_index do |char, index|
   value = char == '#'
-  states[index - 1] = value
+  game.pots[index] = value
 end
 
-known_sequences = []
-
-(1..50000000000).each do |g|
-  new_gen = {}
-  states.each do |pot, state|
-    l2 = states[pot - 2] || false
-    l1 = states[pot - 1] || false
-
-    r1 = states[pot + 1] || false
-    r2 = states[pot + 2] || false
-
-    neighbors  = [l2, l1, state, r1, r2]
-    new_gen[pot] = rules[neighbors] || false
-  end
-  last = new_gen.max_by { |k,v| k }
-  new_gen[last[0] + 1] = false if new_gen[last[0]]
-
-  first = new_gen.min_by { |k,v| k }
-  if new_gen[first[0]]
-    new_gen[first[0] - 1] = false
-  else
-    new_gen.delete(first[0]) unless new_gen[first[0] + 1]
-  end
-
-  this_score = new_gen.select { |k,v| v }.map { |k,v| k }.sum
-  puts "Part 1: #{this_score}" if g == 20
-
-  if known = known_sequences.find { |sequence| sequence.values == new_gen.values }
-    last_score = known.select { |k,v| v }.map { |k,v| k }.sum
-    puts "Part 2: #{this_score + (50000000000 - g) * (this_score - last_score)}"
-    break
-  else
-    known_sequences << new_gen
-  end
-
-  states = new_gen
-end
-
-
+puts "Part 1: #{game.cycle(20)}"
+puts "Part 2: #{game.cycle(50000000000)}"
 
