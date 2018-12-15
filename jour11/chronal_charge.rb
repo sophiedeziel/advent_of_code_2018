e763fc906f3e35c8a1649fcf60a9339ce890e65f
Sophie Déziel <courrier@sophiedeziel.com>
2018-12-14 20:27:46 -0500

    utilisation de summed-area table

--- jour11/chronal_charge.rb
+++ jour11/chronal_charge.rb
@@ -1,37 +1,59 @@
 require 'pry'
-input = 8979
+class Point
+  attr_accessor :x, :y, :value, :sum
 
-size = 300
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @value = initial_value
+    @sum   = integral_value
+  end
 
-@grid = Array.new(size) { Array.new(size, {}) }
+  def square_value(size)
+    return 0 if @x > 300 - size
+    return 0 if @y > 300 - size
+    a = $grid[@x - 1][@y - 1]
+    b = $grid[@x + size - 1][@y - 1]
+    c = $grid[@x - 1][@y + size - 1]
+    d = $grid[@x + size - 1][@y  + size - 1]
+    d.sum - b.sum - c.sum + a.sum
+  end
 
-(1..size).to_a.reverse_each do |x|
-  (1..size).to_a.reverse_each do |y|
+  def initial_value
+    rack_id = @x + 10
+    hundreds = ((rack_id * @y + $sn) * rack_id).digits[2]
+    hundreds - 5
+  end
 
-    rack_id = x + 10
-    hundreds = ((rack_id * y + input) * rack_id).digits[2]
-    @grid[x - 1][y - 1] =
-      {
-        value: hundreds - 5,
-        x: x,
-        y: y,
-        sum: {}
-      }
-
-    (1..20).each do |n|
-      if (y <= size - (n-1)) && (x <= size - (n-1))
-        @grid[x - 1][y - 1][:sum][n] = (x..(x + (n-1))).flat_map { |ax| (y..(y + (n-1))).map { |ay| @grid[ax - 1][ay - 1][:value] } }.sum
-      end
-    end
+  def integral_value
+    return 0 if @x == 0
+    return 0 if @y == 0
+    a = $grid[@x][@y - 1].sum
+    b = $grid[@x - 1][@y].sum
+    c = $grid[@x - 1][@y - 1].sum
+    a + b + @value - c
   end
 end
 
-#puts @grid.map { |line| line.map{|hash| hash[:value]}.join(', ') }.join("\n")
+$sn = 8979
+size = 300
+$grid = Array.new(size) { Array.new(size, nil) }
 
-max = @grid.flatten.max_by { |hash| hash[:sum][3] || 0 }
-#puts max
-puts "Part 1: #{max[:x]},#{max[:y]}"
+(0...size).each do |x|
+  (0...size).each do |y|
+    $grid[x][y] = Point.new(x, y)
+  end
+end
 
-max = @grid.flatten.max_by { |hash| hash[:sum].max_by{ |k, v| v }[1] || 0 }
-puts "Part 2:"
-puts max
+#puts $grid.map { |line| line.map{|point| point.value }.join(', ') }.join("\n")
+#puts $grid.map { |line| line.map{|point| point.sum }.join(', ') }.join("\n")
+
+valid_points = $grid.flatten.reject { |point| point.x == 0 || point.y == 0 }
+
+max = valid_points.max_by { |point| point.square_value(3) }
+puts "Part 1: #{max.x},#{max.y}"
+
+max = valid_points.map do|point|
+  [point, (1..300).max_by { |size| point.square_value(size) }]
+end.max_by { |point, size| point.square_value(size) }
+puts "Part 2: #{max[0].x},#{max[0].y},#{max[1]}"
